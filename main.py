3c0d834b69d1130363b40fb740efa55dd2b74f0d
Masahiko.Toyoshi <Masahiko.Toyoshi@sony.com>
2017-09-25 14:47:48 +0900

    fix function names.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import argparse
-import os
-import glob
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from dataset import create_dataset
 
 
-def parseArguments():
+def parse_argument():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', default='kitti')
     parser.add_argument('--path', required=True)
@@ -14,59 +13,9 @@ def parseArguments():
     return parser.parse_args()
 
 
-class CameraParameters():
-    def __init__(self, fx, fy, cx, cy):
-        self.fx = fx
-        self.fy = fy
-        self.cx = cx
-        self.cy = cy
-
-    @property
-    def cameraMatrix(self):
-        matrix = np.array([[self.fx, 0.0, self.cx],
-                           [0.0, self.fx, self.cy],
-                           [0.0, 0.0, 1.0]])
-        return matrix
-
-    def __call__(self):
-        return self.cameraMatrix
-
-
-class Dataset():
-    def __init__(self):
-        pass
-
-    def image_path_left(self, index):
-        return os.path.join(self.path, self.image_format_left).format(index)
-
-    def count_image(self):
-        extension = os.path.splitext(self.image_format_left)[-1]
-        wildcard = os.path.join(self.path, '*' + extension)
-        self.image_count = len(glob.glob(wildcard))
-
-    def loadCameraParameters(self, calibfile):
-        with open(calibfile, 'r') as f:
-            line = f.readline()
-            part = line.split()
-            param = CameraParameters(part[1], part[6], part[3], part[7])
-
-            return param
-
-
-class KittiDataset(Dataset):
-    def __init__(self, path):
-        self.image_format_left = '{:06d}.png'
-        self.path = path
-        self.calibfile = os.path.join(self.path, 'calib.txt')
-        self.count_image()
-
-
-dataset_dict = {'kitti': KittiDataset}
-
-
 def main():
-    options = parseArguments()
-    dataset = dataset_dict[options.dataset](options.path)
+    options = parse_argument()
+    dataset = create_dataset(options)
 
     feature_detector = cv2.FastFeatureDetector_create(threshold=25,
                                                       nonmaxSuppression=True)
